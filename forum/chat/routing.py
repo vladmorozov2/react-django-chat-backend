dcc657817b5b318d49ae8527e27b0d0087645171
Morozov Vladyslav <vladmorozov@knu.ua>
2025-02-09 19:32:39 +0200

    added group communication

--- forum/chat/routing.py
+++ forum/chat/routing.py
@@ -9,8 +9,9 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "forum.settings")
 from django.urls import re_path
 from . import consumers
 
+from django.urls import re_path
+from . import consumers
+
 websocket_urlpatterns = [
-    re_path(
-        r"ws/chat/", consumers.ChatConsumer.as_asgi()
-    ),  # Match this with your consumer's URL path
+    re_path(r"ws/chat/(?P<room_name>\w+)/$", consumers.ChatConsumer.as_asgi()),
 ]
